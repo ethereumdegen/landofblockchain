b45ea36208bd7d2a964004cc2bb24a5020c9470a
andy <andy@jonezie.com>
2017-09-07 20:02:20 -0400

    now the box is spawned by the server using sockets !

--- desktop/public/js/revolt-player.js
+++ desktop/public/js/revolt-player.js
@@ -12,6 +12,9 @@ let localPlayer = {}
   socket.on('spawnEntity', function (data) {
     console.log('spawning entity');
     console.log(data);
+
+    spawnEntity(data)
+
   });
 
 
@@ -25,7 +28,27 @@ let localPlayer = {}
     p.y = spawnLoc.y;
   }
 
+
   function getSpawnLoc()
   {
     return {x: 5.0, y: 5.0}
+  }
+
+
+  //https://aframe.io/docs/0.5.0/introduction/javascript-events-dom-apis.html
+  function spawnEntity(data)
+  {
+    var sceneEl = document.querySelector('a-scene');
+    var entityEl = document.createElement('a-entity');
+
+
+    // Do `.setAttribute()`s to initialize the entity.
+    entityEl.setAttribute('id',data.entity_id)
+    entityEl.setAttribute('geometry',data.geometry)
+    entityEl.setAttribute('material',data.material)
+    entityEl.setAttribute('position',data.position)
+    entityEl.setAttribute('rotation',data.rotation)
+
+
+    sceneEl.appendChild(entityEl);
   }
