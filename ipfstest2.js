1d413948985834542de45d494e588f3f0d27da6b
andy <andy@jonezie.com>
2017-09-10 20:50:50 -0400

    got test case working !!

---
+++ ipfstest2.js
@@ -0,0 +1,75 @@
+
+const IPFS = require('ipfs')
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
+
+console.log('ipfs test ')
+
+
+//var mhash = "QmRZZJ7soUkzFhFPEtgXgjh7WyMfHTrQz8vaCS57rXorma";
+
+
+// Create the IPFS node instance
+const node = new IPFS()
+
+node.on('ready', () => {
+
+  // Your node is now ready to use \o/
+
+
+    console.log('NODE READY')
+
+    const files = [
+  {
+    path: '/public/sample_land_file.json',
+    content:  fs.readFileSync( path.join(__dirname, '.', '/public/sample_land_file.json') ) // Buffer.from('some data')
+  }
+]
+
+node.files.add(files, function (err, files) {
+  // 'files' will be an array of objects
+  console.log('meep1')
+    console.log(files)
+})
+
+
+
+var wstream =  fs.createWriteStream(os.tmpdir() + '/lobc_cache/'+'Qmc5LfkMVAhvzip2u2RjRBRhgVthtSokHsz4Y5bgaBCW2R');
+
+node.files.cat("QmaF9f8ifiRbefogmdnpvTsjWQ9oMaoPnEJWUaVBjaJbuu", function (err, filestream) {
+    console.log('err')
+    console.log(err)
+
+
+
+      console.log('WHY ISNT CAT FIRING ')
+    console.log(filestream)
+
+
+    wstream.on('finish', function() {
+     console.log('Written ' + wstream.bytesWritten + ' ' + wstream.path);
+       filestream.close() ;
+    });
+
+     filestream.pipe(wstream);
+
+
+
+})
+
+
+
+  // stopping a node
+  node.stop(() => {
+    // node is now 'offline'
+  })
+})
+
+
+node.on('start', () => {
+
+  console.log('NODE START')
+
+
+})
